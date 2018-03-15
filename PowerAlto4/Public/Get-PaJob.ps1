c1148bdbf5a66c607af3132a822338c49fdfad46
Brian Addicks <brian@addicks.us>
2018-03-15 16:29:22 -0400

    Added -Wait to Get-PaJob

--- PowerAlto4/Public/Get-PaJob.ps1
+++ PowerAlto4/Public/Get-PaJob.ps1
@@ -10,7 +10,13 @@ function Get-PaJob {
 
 	Param (
         [Parameter(Mandatory=$False,Position=0)]
-		[int]$JobId
+        [int]$JobId,
+        
+        [Parameter(Mandatory=$False)]
+		[switch]$Wait,
+        
+        [Parameter(Mandatory=$False)]
+		[switch]$ShowProgress
 	)
 
     BEGIN {
@@ -38,12 +44,28 @@ function Get-PaJob {
             $Job.Type         = $result.type
             $Job.Status       = $result.status
             $Job.Result       = $result.result
-            $Job.TimeComplete = Get-Date $result.tfin
             $Job.Warnings     = $result.warnings
             $Job.Details      = $result.details.line -join "`r`n"
             $Job.Description  = $result.description
             $Job.User         = $result.user
             $Job.Progress     = $result.progress
+            if ($Job.Progress -eq 100) {
+                $Job.TimeComplete = Get-Date $result.tfin
+            }
+        }
+
+        if (($Wait -or $ShowProgress) -and ($Job.Progress -ne 100)) {
+            Write-Verbose "$VerbosePrefix Job not complete"
+            
+            # Wait 10 seconds and check again
+            do {
+                Start-Sleep -Seconds 10
+                Write-Verbose "$VerbosePrefix Checking again"
+                $Job = Get-PaJob -JobId $JobId
+                Write-Verbose "$VerbosePrefix Progress: $($Job.Progress)"
+
+            } while ($Job.Progress -ne 100)
+
         }
 
         $ReturnObject
